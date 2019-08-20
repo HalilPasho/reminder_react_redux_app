004a70f964eeecb0f8aeec1ba4dbad8514eaa338
Halil Pasho <halil.pasho@moveoalbania.com>
2019-08-20 09:55:15 +0200

    added react-redux

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -6,8 +6,9 @@ const Header = () => (
   <header className="header">
     <ul className="main-menu">
       {/* <li className="main-menu__item"><Link to="/">Home</Link></li> */}
-      <li className="main-menu__item"><Link to="/calendar">Reminders Calendar</Link></li>
+      {/* <li className="main-menu__item"><Link to="/calendar">Reminders Calendar</Link></li> */}
       {/* <li className="main-menu__item"><Link to="/about">About</Link></li> */}
+      calendar app
     </ul>
   </header>
 );
